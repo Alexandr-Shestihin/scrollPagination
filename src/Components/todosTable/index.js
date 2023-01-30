6878cac6824e971d9d150388281a14c7e20e4f6c
Alexandr-Shestihin <70847183+Alexandr-Shestihin@users.noreply.github.com>
2023-01-30 20:33:54 +0300

    30.01.2023 new function

--- src/Components/todosTable/index.js
+++ src/Components/todosTable/index.js
@@ -2,24 +2,22 @@ import React from 'react';
 import s from './TodosTable.module.css';
 
 const TodosTable = (props) => {
-
+   console.log(props.todos)
    return (
       <div>
          <div className={s.tableTodos}>
             <div>UserId</div>
             <div>Title</div>
-            <div>Id</div>
+            <div>Completed</div>
          </div>
          {props.todos.map(el => <div key={el.id} className={s.tableTodos}>
             <div>{el.userId}</div>
+            <div>{el.title}</div>
             <div>
                <select>
-                  <option value={el.title}>{el.title}</option>
-                  <option value="Other data 1">Other data 1</option>
-                  <option value="Other data 2">Other data 2</option>
-               </select>
-            </div>
-            <div>{el.id}</div>
+                  <option value={el.completed}>{el.completed ? 'Yes' : 'No'}</option>
+                  <option value={!el.completed}>{!el.completed ? 'Yes' : 'No'}</option>
+               </select></div>
          </div>)}
 
       </div>
